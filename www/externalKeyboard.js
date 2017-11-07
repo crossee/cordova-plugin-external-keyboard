883f561e88eb3aec69b5495c58e1e8f7855dbaa3
crossee <a01064297102@gmail.com>
2017-11-07 15:05:31 +0900

    change a running js part

--- www/externalKeyboard.js
+++ www/externalKeyboard.js
@@ -8,7 +8,7 @@
     };
     ExternalKeyboard.isUsed = false;
 
-    ExternalKeyboard.check = function() {
+    ExternalKeyboard.checkOnce = function() {
        var self = this;
 
        exec(function(winParam){
@@ -22,38 +22,30 @@
             );
        }
 // 수시로 확인할 경우 사용
-//       ExternalKeyboard.check = function() {
-//
-//          var action = Object.assign({},
-//              {
-//                 use : function() { console.log("use func not exist"); },
-//                 unuse : function() { console.log("unuse func not exist"); }
-//              },
-//              arg||{});
-//
-//
-//           exec(function(winParam){
-//                if (winParam) {
-//                    typeof action.use === "function" && action.use();
-//                } else {
-//                    typeof action.unuse === "function" && action.unuse();
-//                }
-//            },
-//            function(error){ console.log(error); },
-//            "CDVExternalKeyboardNotification",
-//            "isExternalKeyboardAttached",
-//            []
-//            );
-//       }
-//
-// study.js 에서 initKeyBoard 부분을 아래와 같이 사용
-//               ExternalKeyboard.check({
-//                   use: function() {
-//                       외부 키보드가 사용될 경우 실행될 코드...
-//                   },
-//                   unuse: function() {
-//                       외부 키보드가 없을 경우 실행될 코드...
-//                   }
-//                });
+    ExternalKeyboard.check = function(arg) {
+       var self = this;
+       var action = Object.assign({},
+           {
+              use : function() { console.log("use func not exist"); },
+              unuse : function() { console.log("unuse func not exist"); }
+           },
+           arg||{});
+
+
+        exec(function(winParam){
+             self.isUsed = winParam;
+             console.log(self.isUsed);
+             if (self.isUsed) {
+                 typeof action.use === "function" && action.use();
+             } else {
+                 typeof action.unuse === "function" && action.unuse();
+             }
+         },
+         function(error){ console.log(error); },
+         "ExternalKeyboard",
+         "isExternalKeyboardAttached",
+         []
+         );
+    }
 
     module.exports = ExternalKeyboard;
